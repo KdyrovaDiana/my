87289f11500fda73880190906a3bbab11c96deca
kdyrovadiana <dianablazenec@gmail.com>
2024-09-12 23:19:35 +0300

    add

---
+++ n_1_taymer.py
@@ -0,0 +1,11 @@
+# Исправила проблему с буферизацией очистив поток добавив flush=True в коде
+
+import time
+
+num_seconds = 3
+for countdown in reversed(range(num_seconds + 1)):
+    if countdown > 0:
+        print(countdown, end=' ... ', flush=True)
+        time.sleep(1)
+    else:
+        print('Go!')
